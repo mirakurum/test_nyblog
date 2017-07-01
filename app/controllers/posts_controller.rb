69f7ab01df841eec408f1a8c32c2665399006129
mirakuru <mirakuru.m@yandex.ru>
2017-07-01 20:26:23 +0400

    add rest construction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:posts).permit(:title, :summary, :body)
+  end
+
 end
